61e6bf56cf1387fc082e542db6a423d13f2632b0
brenorusciolelli <brenorusciolelli@gmail.com>
2020-10-28 12:59:51 -0300

    findAll Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findAllt =====");
+		
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
